3b84478384e1561d85ba117f35f135ee1802a165
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2025-07-11 11:05:23 +0300

    feat/feat-change-end-point

--- src/views/CPMGraph.vue
+++ src/views/CPMGraph.vue
@@ -82,7 +82,7 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue'
 import go from 'gojs'
-import axios from 'axios'
+import { api } from '@/config/axiosConfig'
 
 const diagramDiv = ref(null)
 let diagram = null
@@ -160,7 +160,7 @@ async function generateCPM() {
   if (!originalData) {
     try {
 
-      const { data } = await axios.get('http://localhost:8006/api/critical-path')
+      const { data } = await api.get('/critical-path')
       originalData = data
     } catch (error) {
       originalData = {
